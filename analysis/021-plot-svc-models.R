741deaac0fec8bf2e1ba94f7eb5d7caeb7d80e5f
Sean Anderson <sean@seananderson.ca>
2024-05-01 18:43:01 -0700

    Cleaning

--- analysis/021-plot-svc-models.R
+++ analysis/021-plot-svc-models.R
@@ -47,12 +47,17 @@ names(p) <- c("Combined", names(preds))
 apply_rotation_df <- function(dd) {
   dd$X <- NULL
   dd$Y <- NULL
-  dd <- sdmTMB::add_utm_columns(dd, ll_names = c("longitude", "latitude"), utm_crs = 32612, units = "m")
+  dd <- sdmTMB::add_utm_columns(dd,
+    ll_names = c("longitude", "latitude"), utm_crs = 32612, units = "m"
+  )
   rotate_coords(
     dd$X, dd$Y, 25,
-    c(mean(dd$X),
+    c(
+      mean(dd$X),
       mean(dd$Y)
-    )) |> rename(rotated_x = x, rotated_y = y) |>
+    )
+  ) |>
+    rename(rotated_x = x, rotated_y = y) |>
     bind_cols(dd)
 }
 
@@ -69,7 +74,7 @@ pr2 <- c(
 )
 prsl <- lapply(seq_along(pr), \(i) {
   d <- pr2[[i]]
-  d$rotated_x <- d$rotated_x - 750000 * (i-1)
+  d$rotated_x <- d$rotated_x - 750000 * (i - 1)
   d
 })
 names(prsl) <- names(pr2)
@@ -120,39 +125,42 @@ for (i in seq_len(dim(coast_proj4)[1])) {
   )
 }
 rotated_coast <- do.call(rbind, rotated_coast)
-# plot(st_geometry(rotated_coast))
 
 LIMS <- c(0.05, 2)
 LAB <- "Proportion\nbiomass change\nper decade"
+
+# main plot ---------------------------------------------------------------
 
 g <- prs |>
   mutate(svc = ifelse(exp(svc) >= LIMS[2], log(LIMS[2] - 1e-6), svc)) |>
   mutate(svc = ifelse(exp(svc) <= LIMS[1], log(LIMS[1] + 1e-6), svc)) |>
   ggplot(aes(rotated_x, rotated_y, fill = exp(svc), colour = exp(svc))) +
   theme_void() +
-  theme(legend.position = "inside", legend.position.inside = c(0.12, 0.5), legend.title = element_text(colour = "grey30", size = 10), plot.background = element_rect(fill = "white", linewidth = 0)) +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.12, 0.5),
+    legend.title = element_text(colour = "grey30", size = 10),
+    plot.background = element_rect(fill = "white", linewidth = 0)
+  ) +
   geom_sf(data = rotated_coast, inherit.aes = FALSE, fill = "grey70", colour = "grey40") +
-  # geom_point()
   geom_tile(width = 3000, height = 3000) +
-  # scale_fill_gradient2(trans = "log10", limits = c(0.02, 3)) +
-  # scale_fill_gradient2(trans = "log10") +
-
-  # scale_fill_gradient2(trans = "log10", mid = pal[2], low = pal[3], high = pal[1]) +
-  # scale_colour_gradient2(trans = "log10", mid = pal[2], low = pal[3], high = pal[1]) +
-
-  # scale_fill_viridis_c(trans = "log10") +
-  # scale_colour_viridis_c(trans = "log10") +
-
-  colorspace::scale_colour_continuous_divergingx(palette = 'RdBu', mid = 0, trans = "log10",
-    limits = LIMS) +
-  colorspace::scale_fill_continuous_divergingx(palette = 'RdBu', mid = 0, trans = "log10",
-    limits = LIMS) +
-
+  colorspace::scale_colour_continuous_divergingx(
+    palette = "RdBu", mid = 0, trans = "log10",
+    limits = LIMS
+  ) +
+  colorspace::scale_fill_continuous_divergingx(
+    palette = "RdBu", mid = 0, trans = "log10",
+    limits = LIMS
+  ) +
   labs(fill = LAB, colour = LAB) +
-  coord_sf(xlim = c(min(prs$rotated_x), 1546730 - 1800000), ylim = range(prs$rotated_y) + c(0, 100000)) +
-  # guides(fill = "none", colour = "none") +
-  geom_text(data = mids, mapping = aes(x = mean_x - 1000000, label = group_clean),
-    y = 8500000, inherit.aes = FALSE, colour = "grey30", vjust = 1, hjust = 0)
+  coord_sf(
+    xlim = c(min(prs$rotated_x), 1546730 - 1800000),
+    ylim = range(prs$rotated_y) + c(0, 100000)
+  ) +
+  geom_text(
+    data = mids, mapping = aes(x = mean_x - 1000000, label = group_clean),
+    y = 8500000, inherit.aes = FALSE, colour = "grey30", vjust = 1, hjust = 0
+  )
 
 ggsave("figs/svc-trawl.pdf", width = 7, height = 5.5)
 ggsave("figs/svc-trawl.png", width = 7, height = 5.5)
