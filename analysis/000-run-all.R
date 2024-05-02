f5ffb944caecde86679176ebbb091bef1585808b
Sean Anderson <sean@seananderson.ca>
2024-05-01 18:47:44 -0700

    Tidy names

--- analysis/000-run-all.R
+++ analysis/000-run-all.R
@@ -1,21 +1,20 @@
-dir.create("data-generated", showWarnings = FALSE)
+dir.create("output", showWarnings = FALSE)
 dir.create("figs", showWarnings = FALSE)
 
 # main coast-wide index models:
 source("analysis/010-overall-trawl-model.R")
 source("analysis/010-overall-longline-model.R")
-source("analysis/011-fig1.R")
-source("analysis/012-param-tables.R")
+source("analysis/011-plot-overall-trends.R")
+source("analysis/012-plot-coefs.R")
 
 # biomass-weighted depth:
-source("analysis/012-weighted-mean-depth.R")
-source("analysis/013-plot-mean-depth.R")
+source("analysis/013-calculate-weighted-mean-depth.R")
+source("analysis/014-plot-mean-depth.R")
 
-# overall SVC:
+# SVC trends:
 source("analysis/020-svc-models.R")
 source("analysis/021-plot-svc-models.R")
 
 # maturity-stage based:
 source("analysis/030-maturity-models.R")
-source("analysis/031-plot-maturity-models.R")
-source("analysis/031-plot-maturity-svc.R")
+source("analysis/031-plot-maturity-trends.R")
