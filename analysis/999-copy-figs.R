189f2036dac4436307bba2c9f4672d9f76c6c4f7
Sean Anderson <sean@seananderson.ca>
2024-05-08 16:20:39 -0700

    Copy figs

---
+++ analysis/999-copy-figs.R
@@ -0,0 +1,30 @@
+optipng <- function() {
+  wd <- getwd()
+  on.exit(setwd(wd))
+  if (!gfplot:::is_windows()) {
+    setwd("figs")
+    files_per_core <- 2
+    cores <- 10
+    system(paste0(
+      "find -X . -name '*.png' -print0 | xargs -0 -n ",
+      files_per_core, " -P ", cores, " optipng -strip all"
+    ))
+  }
+}
+fcopy <- function(x) {
+  x <- gsub("figs\\/", "", x)
+  file.copy(paste0("figs/", x), paste0("../dogfish-trends-ms/figs/", x), overwrite = TRUE)
+}
+
+optipng()
+f <- c(
+  "figs/overall-survey-trends2.pdf",
+  "figs/svc-trawl-stacked-start.png",
+  "figs/qq-trawl-main.png",
+  "figs/index-trawl-main-by-region-catchability-effects.pdf",
+  "figs/qq-iphc-main.png",
+  "figs/biomass-weighted-depth.pdf",
+  "figs/maturity-index-trends-combo.pdf"
+)
+
+sapply(f, fcopy)
