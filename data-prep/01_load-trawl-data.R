69ab9645af7d7ff762d5ac0e8c3fcff9fe3392aa
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-05-10 15:01:04 -0700

    small changes to code format

--- data-prep/01_load-trawl-data.R
+++ data-prep/01_load-trawl-data.R
@@ -1,4 +1,4 @@
-# load trawl survey data
+# load trawl survey data from Alaska, United States West Coast, and Canada
 
 
 # library -----------------------------------------------------------------
@@ -25,6 +25,7 @@ if (!file.exists("data-raw/data_surveysets.rds")) {
 
 
 # Sets - US West Coast data  ---------------------------------------------------------
+
 # nwfsc triennial survey ran from 1980 - 2004 (1980, 1983, 1986, 1989, 1992, 1995, 1998, 2001, 2004)
 # nwfsc west coast groundfish bottom trawl (WCGFBT) here called combo survey ran from 2003 - 2021 annually except for 2020
 
@@ -87,9 +88,8 @@ if (!file.exists("data-raw/nwfsc_samp_slope_AFSC.rds")) {
   )
   nwfsc_samp_slope_AFSC$Lengths <- nwfsc_samp_slope_AFSC$Lengths %>% mutate(date = as.Date(Date))
   saveRDS(nwfsc_samp_slope_AFSC, "data-raw/nwfsc_samp_slope_AFSC.rds")
-  #nwfsc_samp_slope_AFSC$Lengths %>% group_by(Year) %>% summarise(n = n())
+  # nwfsc_samp_slope_AFSC$Lengths %>% group_by(Year) %>% summarise(n = n())
   # nwfsc_samp_shelf <- nwfscSurvey::PullBio.fn(Name = "Pacific spiny dogfish", SurveyName = "NWFSC.Shelf") # no data
-
 } else {
   nwfsc_samp_combo <- readRDS("data-raw/nwfsc_samp_combo.rds")
   nwfsc_samp_triennial <- readRDS("data-raw/nwfsc_samp_triennial.rds")
@@ -103,7 +103,7 @@ saveRDS(nwfsc_samples_raw, "data-raw/NWFSC_sampledata.rds")
 
 # Sets - Alaskan data ----
 
-#from surveyjoin github
+# from surveyjoin github
 # https://github.com/DFO-NOAA-Pacific/surveyjoin/tree/main/data
 
 load("data-raw/surveyjoin/afsc_haul.rda")
@@ -123,7 +123,7 @@ goa_all_catch <- afsc_catch %>%
 saveRDS(goa_all_sets, "data-raw/goa-sets.rds")
 saveRDS(goa_all_catch, "data-raw/goa-catch.rds")
 
-# Samps - Alaskan data -----------------------------------------------------------
+# Samps - Alaskan data ----
 
 # sample data from: https://github.com/afsc-gap-products/data-requests/issues/62
 # set data from: https://github.com/afsc-gap-products/gap_public_data#access-the-data
