02a7513b4d04d8ee652ef96e1ff61ffdeffb0cc3
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-05-10 15:01:42 -0700

    small cleaning changes

--- data-prep/01-load-IPHC-data.R
+++ data-prep/01-load-IPHC-data.R
@@ -1,11 +1,8 @@
-# Pull IPHC data
-
-
-# CHECK IPHC coast data clean - get rid of expansion set, add date, checks --------------------------------------------------
+# Load IPHC data
 
 # downloaded from here:
-
 # https://www.iphc.int/data/fiss-data-query
+
 iphc_stations <- read.csv("data-raw/IPHC data download from IPHC website/Map select_standardgrid.csv")
 iphc_coast <- read.csv("data-raw/IPHC data download from IPHC website/Non-Pacific halibut data_raw.csv")
 iphc_latlongs <- read.csv("data-raw/IPHC data download from IPHC website/Set and Pacific halibut data_raw.csv") %>%
