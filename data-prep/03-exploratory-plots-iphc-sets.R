6f62de2cd0cf1137f6189f905766bd08c1cc4ed0
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-05-13 10:18:20 -0700

    cleaned code

--- data-prep/03-exploratory-plots-iphc-sets.R
+++ data-prep/03-exploratory-plots-iphc-sets.R
@@ -1,13 +1,13 @@
 library(ggplot2)
 library(dplyr)
 
-
+#from 02-wrange-IPHC-data.R
 iphc <- readRDS("output/IPHC_coastdata.rds")
+
 
 ggplot(iphc, aes(as.factor(year), julian)) +
   geom_boxplot() +
   facet_wrap(~iphc.reg.area, nrow = 1)
-
 
 iphc %>%
   group_by(year, iphc.reg.area) %>%
@@ -16,7 +16,6 @@ iphc %>%
   geom_line(size = 2) +
   geom_point(size = 2, colour = "red") +
   facet_wrap(~iphc.reg.area)
-
 
 iphc %>%
   group_by(year, iphc.reg.area) %>%
@@ -35,17 +34,15 @@ iphc |>
   geom_point(aes(UTM.lon.m, UTM.lat.m), colour = "grey") +
   facet_wrap(~year)
 
-x <- ggplot(iphc, aes(UTM.lon.m, UTM.lat.m), colour = "grey") +
+ggplot(iphc, aes(UTM.lon.m, UTM.lat.m), colour = "grey") +
   geom_point() +
   facet_wrap(~year)
-
 
 ggplot(
   data = iphc_coast7,
   aes(UTM.lon, UTM.lat), size = 1.5, col = "blue"
 ) +
   geom_point()
-
 
 ggplot(iphc3, aes(longitude, latitude, colour = diff)) +
   geom_point(size = 0.4) +
