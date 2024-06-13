e85e4bbf4fc8b47dedbaabc1c833a637a1e5711e
Sean Anderson <sean@seananderson.ca>
2024-06-13 10:58:46 -0700

    Add run all for data prep

---
+++ data-prep/00-run-all.R
@@ -0,0 +1,14 @@
+dir.create("output", showWarnings = FALSE)
+dir.create("figs", showWarnings = FALSE)
+
+source("data-prep/01-load-trawl-data.R")
+source("data-prep/02-wrangle-IPHC-data.R")
+source("data-prep/02-wrangle-sample-trawl-data.R")
+source("data-prep/02-wrangle-sets-trawl-data.R")
+source("data-prep/03-exploratory-plots-iphc-sets.R")
+## source("data-prep/04-SOM-figures-raw-survey-data.R")
+source("data-prep/05-split-index-by-region-maturity.R")
+## source("data-prep/06-load-cpue-data.R")
+## source("data-prep/07-wrange-cpue-data.R")
+## source("data-prep/08-exploratory-plots-cpue.R")
+
