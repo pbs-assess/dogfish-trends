0db1b9aacb149dba3209795545c7f0e8e041efc7
Sean Anderson <sean@seananderson.ca>
2024-06-12 17:56:10 -0700

    Delete extra files

--- data-prep/01-load-IPHC-data.R
+++
@@ -1,9 +0,0 @@
-# Load IPHC data
-
-# downloaded from here:
-# https://www.iphc.int/data/fiss-data-query
-
-iphc_stations <- read.csv("data-raw/IPHC data download from IPHC website/Map select_standardgrid.csv")
-iphc_coast <- read.csv("data-raw/IPHC data download from IPHC website/Non-Pacific halibut data_raw.csv")
-iphc_latlongs <- read.csv("data-raw/IPHC data download from IPHC website/Set and Pacific halibut data_raw.csv") %>%
-  dplyr::select(IPHC.Reg.Area, Date, Eff, Ineffcde, BeginLat, BeginLon, AvgDepth..fm., Stlkey)
