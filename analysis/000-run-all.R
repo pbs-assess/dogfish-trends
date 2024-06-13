83050312f8aa2b996c7f50d0a0e6ea114cc0b92a
Sean Anderson <sean@seananderson.ca>
2024-06-13 12:20:01 -0700

    Update run all script

--- analysis/000-run-all.R
+++ analysis/000-run-all.R
@@ -11,18 +11,21 @@ dir.create("figs", showWarnings = FALSE)
 # main coast-wide index models:
 source("analysis/010-overall-trawl-model.R")
 source("analysis/010-overall-longline-model.R")
-source("analysis/011-plot-overall-trends.R")
-source("analysis/012-plot-coefs.R")
+source("analysis/012-individual-trawl-models.R")
+source("analysis/013-plot-overall-trends.R")
+source("analysis/014-plot-coefs.R")
 
-# biomass-weighted depth:
-source("analysis/013-calculate-weighted-mean-depth.R")
-source("analysis/014-plot-mean-depth.R")
+source("analysis/015-maturity-models.R")
+source("analysis/016-calculate-weighted-mean-depth.R")
+source("analysis/017-plot-mean-depth.R")
 
 # SVC trends:
 source("analysis/020-svc-models.R")
-source("analysis/021-plot-svc-models.R")
-source("analysis/021-plot-svc-models2.R") # has intercept too
+source("analysis/021-plot-svc-models2.R")
 
-# maturity-stage based:
-source("analysis/030-maturity-models.R")
 source("analysis/031-plot-maturity-trends.R")
+source("analysis/040-model-table.R")
+source("analysis/050-plot-maturity-hists-ogive.R")
+
+# values for paper:
+source("analysis/080-values.R")
