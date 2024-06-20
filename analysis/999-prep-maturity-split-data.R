d399b6aef1d2e1e41ae3ba5aaaabdd0526f664cd
ecophilina <paenglis@lakeheadu.ca>
2024-06-19 18:06:33 -0700

    update projection for maturity split data

--- analysis/999-prep-maturity-split-data.R
+++ analysis/999-prep-maturity-split-data.R
@@ -2,7 +2,7 @@ prep_maturity_split_data <- function() {
   d <- readRDS("output/splitbymaturityregion_df.rds")
   d <- dplyr::filter(d, !is.na(longitude) & !is.na(latitude))
   d <- dplyr::filter(d, survey_name != "Triennial")
-  d <- sdmTMB::add_utm_columns(d, c("longitude", "latitude"), units = "km", utm_crs = 32607)
+  d <- sdmTMB::add_utm_columns(d, c("longitude", "latitude"), units = "km", utm_crs = coast_crs)
   d$offset_km <- log(d$area_swept/(1000*1000))
   d$catch_weight_t <- d$catch_weight_ratio # !! catch_weight_ratio; not catch_weight
   d$depth_m <- exp(d$logbot_depth)
