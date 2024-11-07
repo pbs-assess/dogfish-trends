c5f17b4acb61850d89f461a8e5d7dcd22bdc3e44
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-11-07 14:28:41 -0800

    clean figures

--- analysis/014a-plot-catchability-coefs.R
+++ analysis/014a-plot-catchability-coefs.R
@@ -8,27 +8,31 @@ fitq <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-poisson-link-q-", m
 # fit1 <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-",min_edge,"-", max_edge,".rds"))
 fit4 <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-poisson-link-", min_edge, "-", max_edge, ".rds")) # this is the one that was used for the index generation. delta lognormal mix with a poisson link
 
-m1_df <- broom::tidy(fit4)
+#m1_df <- broom::tidy(fit4)
+m1_df <- tidy(fit4, conf.int = TRUE, model = 1) #no surveys
 m1_df
-m2_df <- broom::tidy(fitq)
-m2_df
+
+m2_df <- tidy(fitq, conf.int = TRUE, model = 1)
 m2_df <- m2_df[c(1, 4:6), ]
+m2_df$model <- "model1"
+m2_df2 <- tidy(fitq, conf.int = TRUE, model = 1)
+m2_df2 <- m2_df2[c(1, 4:6), ]
+m2_df2$model <- "model2"
 
-m2_df <- m2_df |>
-  mutate(term = c("BC", "GOA", "WCGBT pass 1", "WCGBT pass 2"))
+m2 <- bind_rows(m2_df, m2_df2)
+m2 <- m2 |>
+  mutate(term2 = c("BC", "GOA", "WCGBT pass 1", "WCGBT pass 2",
+                   "BC", "GOA", "WCGBT pass 1", "WCGBT pass 2"))
 
-dotwhisker::dwplot(m2_df,
-  dot_args = list(size = 2, pch = 21, fill = "red"),
-  vline = geom_vline(xintercept = 0, colour = "grey50", linetype = 2)
-) +
-  xlab("Coefficient") +
-  theme_bw() + xlab("Coefficient") + ylab("") +
-  geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
-  theme(
-    legend.position = "none"
-  )
+ggplot(m2, aes(estimate,term2)) +
+  geom_point() +
+  geom_linerange(aes(xmin = conf.low, xmax = conf.high)) +
+  facet_wrap(~model) +
+  geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
+  xlab("Estimate") + ylab("Coastwide")
 
-ggsave("figs/coastwide-catcability-coeff-plot.png", width = 5, height = 5)
+
+ggsave("figs/coastwide-catchability-coeff-plot.png", width = 5, height = 3)
 
 
 
@@ -39,28 +43,26 @@ ggsave("figs/coastwide-catcability-coeff-plot.png", width = 5, height = 5)
 out <- readRDS(file = "output/fit-trawl-by-region-lognormal-poisson-link-w-catchabilities.rds")
 out$fit
 
-m1_df <- broom::tidy(out$fit)
+m1_df <- tidy(out$fit, conf.int = TRUE, model = 1)
 m1_df$model <- "model1"
 m1_df <- m1_df[c(1:7), ]
-m2_df <- broom::tidy(out$fit, model = 2)
+m2_df <- tidy(out$fit, conf.int = TRUE, model = 2)
 m2_df$model <- "model2"
 m2_df <- m2_df[c(1:7), ]
 
 m <- bind_rows(m1_df, m2_df)
-dotwhisker::dwplot(m1_df)
-dotwhisker::dwplot(m2_df)
-#dotwhisker::dwplot(m2_df, facet_wrap(~model))
+#dotwhisker::dwplot(m1_df)
+#dotwhisker::dwplot(m2_df)
 
 m <- m |>
-  mutate(term2 = c("WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early", "WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early")) |>
-  mutate(upr = estimate  + std.error, lwr = estimate  - std.error)
+  mutate(term2 = c("WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early", "WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early"))
 
-ggplot(m, aes(estimate,term2), colour = "red") +
+ggplot(m, aes(estimate,term2)) +
   geom_point() +
-  geom_linerange(aes(xmin = lwr, xmax = upr)) +
+  geom_linerange(aes(xmin = conf.low, xmax = conf.high)) +
   facet_wrap(~model) +
   geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
   xlab("Estimate") + ylab("US West Coast survey")
 
-ggsave("figs/nwfsc-catcability-coeff-plot.png", width = 5, height = 5)
+ggsave("figs/nwfsc-catchability-coeff-plot.png", width = 5, height = 3)
 
