0bac245c2b4cd4a29301d1033adc94a91c1fecbd
Sean Anderson <sean@seananderson.ca>
2024-11-26 14:59:30 -0800

    Update year to 2023

--- analysis/080-values.R
+++ analysis/080-values.R
@@ -8,7 +8,7 @@ write_tex <- function(x, macro, append = TRUE) {
 }
 
 glmdf <- readRDS("output/glmdf-overall-trends.rds")
-decades <- (2022-2005)/10
+decades <- (2023-2005)/10
 s <- glmdf |> select(region, slope, lwr, upr) |> distinct() |>
   mutate(slope_dec = 100 * (1 - exp(slope * decades)), lwr_dec = 100 * (1 - exp(lwr * decades)), upr_dec = 100 * (1 - exp(upr * decades))) |>
   mutate(slope = exp(slope), lwr = exp(lwr), upr = exp(upr)) |>
