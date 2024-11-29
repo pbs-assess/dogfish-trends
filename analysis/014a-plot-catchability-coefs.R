e0fb03d8f42e6a19c896b4c70b8aeea2b66ef14a
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-11-28 16:23:18 -0800

    clean ggplot

--- analysis/014a-plot-catchability-coefs.R
+++ analysis/014a-plot-catchability-coefs.R
@@ -3,13 +3,13 @@
 
 
 # plot coef for coastwide catachability coef ------------------------------
-#pulled these from 010-overall-trawl-model.R
+# pulled these from 010-overall-trawl-model.R
 fitq <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-poisson-link-q-", min_edge, "-", max_edge, ".rds"))
 # fit1 <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-",min_edge,"-", max_edge,".rds"))
 fit4 <- readRDS(paste0("output/fit-trawl-coast-lognormal-mix-poisson-link-", min_edge, "-", max_edge, ".rds")) # this is the one that was used for the index generation. delta lognormal mix with a poisson link
 
-#m1_df <- broom::tidy(fit4)
-m1_df <- tidy(fit4, conf.int = TRUE, model = 1) #no surveys
+# m1_df <- broom::tidy(fit4)
+m1_df <- tidy(fit4, conf.int = TRUE, model = 1) # no surveys
 m1_df
 
 m2_df <- tidy(fitq, conf.int = TRUE, model = 1)
@@ -24,24 +24,27 @@ m2
 
 m2 <- bind_rows(m2_df, m2_df2)
 m2 <- m2 |>
-  mutate(term2 = c("Intercept", "GOA", "WCGBT pass 1", "WCGBT pass 2",
-                   "Intercept", "GOA", "WCGBT pass 1", "WCGBT pass 2"))
+  mutate(term2 = c(
+    "Intercept", "GOA", "WCGBT pass 1", "WCGBT pass 2",
+    "Intercept", "GOA", "WCGBT pass 1", "WCGBT pass 2"
+  ))
 
 m2 <- m2 %>%
   mutate(term2 = factor(term2, levels = c("WCGBT pass 2", "WCGBT pass 1", "GOA", "Intercept")))
-ggplot(m2, aes(estimate,term2)) +
+ggplot(m2, aes(estimate, term2)) +
   geom_point() +
   geom_linerange(aes(xmin = conf.low, xmax = conf.high)) +
   facet_wrap(~model) +
   geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
-  xlab("Estimate") + ylab("Coastwide")
+  xlab("Estimate") +
+  ylab("Coastwide")
 
 ggsave("figs/coastwide-catchability-coeff-plot.png", width = 5, height = 3)
 
 
 
 # plot coeff for NWFSC catchability model ---------------------------------
-#from 012-individual-trawl-models.R
+# from 012-individual-trawl-models.R
 # plot out different analyses for the longest time series possible
 
 out <- readRDS(file = "output/fit-trawl-by-region-lognormal-poisson-link-w-catchabilities.rds")
@@ -55,18 +58,21 @@ m2_df$model <- "model2"
 m2_df <- m2_df[c(1:7), ]
 
 m <- bind_rows(m1_df, m2_df)
-#dotwhisker::dwplot(m1_df)
-#dotwhisker::dwplot(m2_df)
+# dotwhisker::dwplot(m1_df)
+# dotwhisker::dwplot(m2_df)
 
 m <- m |>
-  mutate(term2 = c("WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early", "WCGBT pass 1", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early"))
+  mutate(term2 = c("Intercept", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early", "Intercept", "WCGBT pass 2", "AFSC slope", "AFSC slope early", "NWFSC slope", "Triennial", "Triennial early"))
 
-ggplot(m, aes(estimate,term2)) +
+m <- m |>
+  mutate(term2 = factor(term2, levels = c("AFSC slope early", "AFSC slope", "NWFSC slope", "Triennial early", "Triennial", "WCGBT pass 2", "Intercept")))
+
+ggplot(m, aes(estimate, term2)) +
   geom_point() +
   geom_linerange(aes(xmin = conf.low, xmax = conf.high)) +
   facet_wrap(~model) +
   geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
-  xlab("Estimate") + ylab("US West Coast survey")
+  xlab("Estimate") +
+  ylab("US West Coast survey")
 
 ggsave("figs/nwfsc-catchability-coeff-plot.png", width = 5, height = 3)
-
