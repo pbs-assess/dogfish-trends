d9653e25e8c42128f95386cb25fc506828884bb7
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-11-29 16:09:47 -0800

    change catch weight units to t

--- analysis/999-prep-maturity-split-data.R
+++ analysis/999-prep-maturity-split-data.R
@@ -4,7 +4,7 @@ prep_maturity_split_data <- function() {
   d <- dplyr::filter(d, survey_name != "Triennial")
   d <- sdmTMB::add_utm_columns(d, c("longitude", "latitude"), units = "km", utm_crs = coast_crs)
   d$offset_km <- log(d$area_swept/(1000*1000))
-  d$catch_weight_t <- d$catch_weight_ratio # !! catch_weight_ratio; not catch_weight
+  d$catch_weight_t <- d$catch_weight_ratio/1000 # !! catch_weight_ratio is in kg
   d$depth_m <- exp(d$logbot_depth)
   d
 }
