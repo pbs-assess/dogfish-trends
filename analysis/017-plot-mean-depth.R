2552e0acc65cbcad0549eb03775158c438434db6
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-12-04 11:55:34 -0800

    added temp plot

--- analysis/017-plot-mean-depth.R
+++ analysis/017-plot-mean-depth.R
@@ -1,10 +1,14 @@
 library(dplyr)
 library(ggplot2)
+#devtools::install_github("seananderson/ggsidekick")
+#devtools::install_github("eliocamp/tagger")
 theme_set(ggsidekick::theme_sleek())
 source("analysis/999-colours-etc.R")
 
-ret <- readRDS("output/biomass-weighted-depth.rds")
-# ret <- readRDS("output/biomass-weighted-depth-pe.rds")
+#ret <- readRDS("output/biomass-weighted-depth.rds")
+ret <- readRDS("output/biomass-weighted-depth-temp.rds") #<- include depth and temp
+
+ret <- filter(ret, maturity_group != "mature")
 ret <- rename(ret, group = maturity_group)
 ret <- add_maturity_group_clean_column(ret)
 ret <- clean_region_names(ret)
@@ -37,3 +41,31 @@ print(g)
 
 ggsave("figs/biomass-weighted-depth.png", width = 7.3, height = 2.8)
 ggsave("figs/biomass-weighted-depth.pdf", width = 7.3, height = 2.8)
+
+g <- ret |>
+  filter(region != "Coastwide") |>
+  ggplot(aes(year, mean_temp , colour = group_clean, fill = group_clean)) +
+  geom_line() +
+  geom_ribbon(aes(
+    ymin = lwr25_temp,
+    ymax = upr75_temp),
+    colour = NA, alpha = 0.3
+  ) +
+  facet_wrap(~region, nrow = 1L) +
+  #scale_y_reverse() +
+  scale_colour_manual(values = cols_maturities) +
+  scale_fill_manual(values = cols_maturities) +
+  labs(
+    fill = "Group", colour = "Group", x = "Year",
+    y = "Biomass-weighted mean temp (C)"
+  ) +
+  theme(legend.position = "inside", legend.position.inside = c(0.80, 0.28), axis.title.x = element_blank()) +
+  # theme(legend.position = "inside", legend.position.inside = c(0.9, 0.75), axis.title.x = element_blank()) +
+  tagger::tag_facets(tag = "panel",
+                     tag_prefix = "(", position = "tl"
+  ) +
+  theme(tagger.panel.tag.text = element_text(color = "grey30", size = 9), axis.title.x = element_blank())
+print(g)
+
+ggsave("figs/biomass-weighted-temp.png", width = 7.3, height = 2.8)
+ggsave("figs/biomass-weighted-temp.pdf", width = 7.3, height = 2.8)
